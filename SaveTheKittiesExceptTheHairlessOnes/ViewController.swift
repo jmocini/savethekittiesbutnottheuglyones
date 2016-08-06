e4c82d5ef4107ecb6f0e4c06984fc8d7c72c83ae
jmocini <JDM@Jeffreys-MacBook-Pro.local>
2016-08-06 13:49:19 -0400

    Made a test print message
    For no reason at all.

--- SaveTheKittiesExceptTheHairlessOnes/ViewController.swift
+++ SaveTheKittiesExceptTheHairlessOnes/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Save the kitties! But not the hairless ones.  They are ugly and mean.")
     }
 
     override func didReceiveMemoryWarning() {
